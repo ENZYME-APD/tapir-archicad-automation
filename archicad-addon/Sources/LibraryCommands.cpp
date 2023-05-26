d7b68f6a1946edcb2c9673a1167e9ebccb208844
iyur <iyurrr@gmail.com>
2023-05-26 20:09:11 +0300

    APIEnv_​GetLibrariesID command (#67)
    * APIEnv_​GetLibrariesID command
    
    * Update AddOnVersion.hpp
    
    * Update LibraryCommands.cpp
    
    * Update LibraryCommands.cpp
    
    * Update LibraryCommands.cpp
    
    * Add documentation and example.
    
    ---------
    
    Co-authored-by: kovacsv <viktorkovacs@gmail.com>

--- archicad-addon/Sources/LibraryCommands.cpp
+++ archicad-addon/Sources/LibraryCommands.cpp
@@ -1,6 +1,134 @@
 #include "LibraryCommands.hpp"
 #include "ObjectState.hpp"
 
+GS::Optional<GS::UniString> GetLibrariesCommand::GetResponseSchema () const
+{
+    return R"({
+        "type": "object",
+        "properties": {
+            "libraries": {
+                "type": "array",
+                "description": "A list of project libraries.",
+                "items": {
+                    "type": "object",
+                    "description": "Library",
+                    "properties": {
+                        "name": {
+                            "type": "string",
+                            "description": "Library name."
+                        },
+                        "path": {
+                            "type": "string",
+                            "description": "A filesystem path to library location."
+                        },
+                        "type": {
+                            "type": "string",
+                            "description": "Library type."
+                        },
+                        "available": {
+                            "type": "boolean",
+                            "description": "Is library not missing."
+                        },
+                        "readOnly": {
+                            "type": "boolean",
+                            "description": "Is library not writable."
+                        },
+                        "twServerUrl": {
+                            "type": "string",
+                            "description": "URL address of the TeamWork server hosting the library."
+                        },
+                        "urlWebLibrary": {
+                            "type": "string",
+                            "description": "URL of the downloaded Internet library."
+                        }
+                    },
+                    "additionalProperties": false,
+                    "required": [
+                        "name",
+                        "type",
+                        "path"
+                    ]
+                }
+            }
+        },
+        "additionalProperties": false,
+        "required": [
+            "libraries"
+        ]
+    })";
+}
+
+GetLibrariesCommand::GetLibrariesCommand () :
+    CommandBase (CommonSchema::NotUsed)
+{
+}
+
+GS::String GetLibrariesCommand::GetName () const
+{
+    return "GetLibraries";
+}
+
+GS::ObjectState GetLibrariesCommand::Execute (const GS::ObjectState& /*parameters*/, GS::ProcessControl& /*processControl*/) const
+{
+    GS::Array<API_LibraryInfo> libs;
+
+    GSErrCode err = ACAPI_Environment (APIEnv_GetLibrariesID, &libs);
+    if (err != NoError) {
+        return CreateErrorResponse (err, "Failed to retrive libraries.");
+    }
+
+    GS::ObjectState response;
+    const auto& listAdder = response.AddList<GS::ObjectState> ("libraries");
+
+    for (UInt32 i = 0; i < libs.GetSize (); i++) {
+
+        GS::ObjectState libraryData;
+        GS::UniString   type;
+        GS::UniString   twServerUrl;
+        GS::UniString   urlWebLibrary;
+        switch (libs[i].libraryType) {
+            case API_LibraryTypeID::API_Undefined:
+                type = "Undefined";
+                break;
+            case API_LibraryTypeID::API_LocalLibrary:
+                type = "LocalLibrary";
+                break;
+            case API_LibraryTypeID::API_UrlLibrary:
+                type = "UrlLibrary";
+                urlWebLibrary = libs[i].twServerUrl;
+                break;
+            case API_LibraryTypeID::API_BuiltInLibrary:
+                type = "BuiltInLibrary";
+                break;
+            case API_LibraryTypeID::API_EmbeddedLibrary:
+                type = "EmbeddedLibrary";
+                break;
+            case API_LibraryTypeID::API_OtherObject:
+                type = "OtherObject";
+                break;
+            case API_LibraryTypeID::API_UrlOtherObject:
+                type = "UrlOtherObject";
+                urlWebLibrary = libs[i].twServerUrl;
+                break;
+            case API_LibraryTypeID::API_ServerLibrary:
+                type = "ServerLibrary";
+                twServerUrl = libs[i].twServerUrl;
+                break;
+        }
+
+        libraryData.Add ("name", libs[i].name);
+        libraryData.Add ("path", libs[i].location.ToDisplayText());
+        libraryData.Add ("type", type);
+        libraryData.Add ("available", libs[i].available);
+        libraryData.Add ("readOnly", libs[i].readOnly);
+        libraryData.Add ("twServerUrl", twServerUrl);
+        libraryData.Add ("urlWebLibrary", urlWebLibrary);
+        listAdder (libraryData);
+    }
+
+    return response;
+}
+
 ReloadLibrariesCommand::ReloadLibrariesCommand () :
     CommandBase (CommonSchema::NotUsed)
 {
