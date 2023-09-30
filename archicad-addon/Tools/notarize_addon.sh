a68cd7a3cdde3523a34f6aa8a2859b9b0e15c4e5
kovacsv <viktorkovacs@gmail.com>
2023-09-30 16:43:26 +0200

    Set entitlements file path.

--- archicad-addon/Tools/notarize_addon.sh
+++ archicad-addon/Tools/notarize_addon.sh
@@ -4,11 +4,11 @@
 P12FilePath=$1
 P12FilePassword=$2
 CodeSignIdentity=$3
-AppleID=$4
-AppleIDPassword=$5
-TeamID=$6
-AddOnBundlePath=$7
-TempZipFileName="BundleForNotarization.zip"
+EntitlementsFilePath=$4
+AppleID=$5
+AppleIDPassword=$6
+TeamID=$7
+AddOnBundlePath=$8
 
 # make sure the script exits on error
 set -e
@@ -19,35 +19,28 @@ security default-keychain -s notarization.keychain
 security unlock-keychain -p TempKeyChainPass notarization.keychain
 
 # import the p12 file into the keychain
-security import $P12FilePath -k notarization.keychain -P $P12FilePassword -T /usr/bin/codesign
+security import "$P12FilePath" -k notarization.keychain -P $P12FilePassword -T /usr/bin/codesign
 
 # make sure that no password prompt will appear
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k TempKeyChainPass notarization.keychain
 
 # sign the bundle
-echo "codesign"
-echo $AddOnBundlePath
-mdls $AddOnBundlePath
-echo $CodeSignIdentity
-echo "codesign end"
-security find-identity
-/usr/bin/codesign -s "$CodeSignIdentity" -f -vvv --deep --timestamp --options runtime "$AddOnBundlePath"
+/usr/bin/codesign -s "$CodeSignIdentity" -f -vvv --deep --timestamp --entitlements "$EntitlementsFilePath" --options runtime "$AddOnBundlePath"
 
 # store credentials for notarization
-echo "credentials"
 xcrun notarytool store-credentials "addon.notarization" --apple-id $AppleID --password $AppleIDPassword --team-id $TeamID
 
+# set file name for the zip to send for notarization
+TempZipFileName="BundleForNotarization.zip"
+
 # create a zip to upload to notarization service
-echo "compress"
-ditto -c -k --keepParent $AddOnBundlePath $TempZipFileName
+ditto -c -k --keepParent "$AddOnBundlePath" "$TempZipFileName"
 
 # submit the zip to the notarization service
-echo "submit"
-xcrun notarytool submit $TempZipFileName --wait --keychain-profile "addon.notarization"
+xcrun notarytool submit "$TempZipFileName" --wait --keychain-profile "addon.notarization"
 
 # staple the ticket to the bundle
-echo "staple"
-xcrun stapler staple $AddOnBundlePath
+xcrun stapler staple "$AddOnBundlePath"
 
 # delete the temporary zip file
-rm $TempZipFileName
+rm "$TempZipFileName"
