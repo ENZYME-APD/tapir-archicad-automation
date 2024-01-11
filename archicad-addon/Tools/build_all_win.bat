67cf8cfca175219d43d9fed45a751c5496af6c2f
kovacsv <viktorkovacs@gmail.com>
2024-01-11 21:20:43 +0100

    Add one unified build script.

---
+++ archicad-addon/Tools/build_all_win.bat
@@ -0,0 +1,18 @@
+pushd %~dp0
+python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/25.3002/API.Development.Kit.WIN.25.3002.zip ..\Build\DevKits\AC25
+python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/26.3000/API.Development.Kit.WIN.26.3000.zip ..\Build\DevKits\AC26
+python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/27.3001/API.Development.Kit.WIN.27.3001.zip ..\Build\DevKits\AC27
+
+cmake -B ../Build/AC25 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_API_DEVKIT_DIR="..\Build\DevKits\AC25" .. || goto :error
+cmake --build ../Build/AC25 --config Debug || goto :error
+
+cmake -B ../Build/AC26 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_API_DEVKIT_DIR="..\Build\DevKits\AC26" .. || goto :error
+cmake --build ../Build/AC26 --config Debug || goto :error
+
+popd
+exit /b 0
+
+:error
+echo Build Failed with Error %errorlevel%.
+popd
+exit /b 1
