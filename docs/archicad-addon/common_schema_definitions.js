a5a0841005adb9d660b83494b56f7649615b740f
Tibor Lorántfy <43233474+tlorantfy@users.noreply.github.com>
2024-07-13 11:30:24 +0200

    Get/Set GDL Object Parameters #34 (#85)

--- docs/archicad-addon/common_schema_definitions.js
+++ docs/archicad-addon/common_schema_definitions.js
@@ -93,5 +93,43 @@
         "required": [
             "location"
         ]
+    },
+    "GDLParametersDictionary": {
+        "type": "object",
+        "description": "The dictionary of GDL parameters. The name of the parameter is the key and the details of the parameter are in the value.",
+        "additionalProperties": {
+            "type": "#/GDLParameterDetails"
+        }
+    },
+    "GDLParameterDetails": {
+        "type": "object",
+        "description": "Details of a GDL parameter.",
+        "properties": {
+            "index": {
+                "type": "string",
+                "description": "The index of the parameter."
+            },
+            "type": {
+                "type": "string",
+                "description": "The type of the parameter."
+            },
+            "dimension1": {
+                "type": "string",
+                "description": "The 1st dimension of array (in case of array value)."
+            },
+            "dimension2": {
+                "type": "string",
+                "description": "The 2nd dimension of array (in case of array value)."
+            },
+            "value": {
+                "description": "The value of the parameter."
+            }
+        },
+        "additionalProperties": true,
+        "required": [
+            "index",
+            "type",
+            "value"
+        ]
     }
 };
