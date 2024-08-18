d6f8b194b8df8b2dcf774345e48d85728cbc3335
Viktor Kovacs <viktorkovacs@gmail.com>
2024-08-18 22:15:03 +0200

    Update version.

--- grasshopper-plugin/src/tapir.py
+++ grasshopper-plugin/src/tapir.py
@@ -44,7 +44,7 @@ class AssemblyInfo(GhPython.Assemblies.PythonAssemblyInfo):
         return """Grasshopper components to access ArchiCAD via JSON API."""
 
     def get_AssemblyVersion(self):
-        return "1.0.6"
+        return "1.0.7"
 
     def get_AuthorName(self):
         return "EnzymeAPD"
