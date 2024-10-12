098879610f2f57045ea98d42180dab95ef30ced4
Viktor Kovacs <viktorkovacs@gmail.com>
2024-10-12 16:39:38 +0200

    Minor modification.

--- grasshopper-plugin/create_package_win.bat
+++ grasshopper-plugin/create_package_win.bat
@@ -10,8 +10,6 @@ xcopy ..\TapirGrasshopperPlugin\bin\Release\net48\*.dll . /s /d /y
 xcopy ..\TapirGrasshopperPlugin\bin\Release\net7.0\*.gha net7.0 /I /s /d /y
 xcopy ..\TapirGrasshopperPlugin\bin\Release\net7.0\*.dll net7.0 /I /s /d /y
 
-call %1 build
-
 popd
 exit /b 0
 
