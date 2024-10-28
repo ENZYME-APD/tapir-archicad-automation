cb547dab7b162a92ec6c3ef94fa56310a879dd5f
Kaushik <50703773+thekaushikls@users.noreply.github.com>
2024-10-28 07:41:32 +0100

    [added] buttons to components (#186)
    * [added] buttons to components
    
    * Make all components archicad components.
    
    ---------
    
    Co-authored-by: Viktor Kovacs <viktorkovacs@gmail.com>

--- grasshopper-plugin/TapirGrasshopperPlugin/Components/Component.cs
+++ grasshopper-plugin/TapirGrasshopperPlugin/Components/Component.cs
@@ -1,11 +1,102 @@
-﻿using Grasshopper.Kernel;
+﻿using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 using TapirGrasshopperPlugin.Utilities;
 
 namespace TapirGrasshopperPlugin.Components
 {
+    public interface IButtonComponent
+    {
+        void OnCapsuleButtonPressed();
+    }
+
+    public class ButtonAttributes : GH_ComponentAttributes
+    {
+        #region Fields
+        private bool _isPressed = false;
+        private RectangleF _buttonBounds = new RectangleF ();
+        #endregion Fields
+
+        #region Constructor
+        public ButtonAttributes (IGH_Component component) : base (component) { }
+        #endregion Constructor
+
+        #region Methods
+        protected override void Layout()
+        {
+            base.Layout ();
+
+            Rectangle componentCapsule = GH_Convert.ToRectangle (this.Bounds);
+            componentCapsule.Height += 30;
+
+            Rectangle buttonCapsule = componentCapsule;
+            buttonCapsule.Y = buttonCapsule.Bottom - 30;
+            buttonCapsule.Height = 30;
+            buttonCapsule.Inflate (-5, -5);
+
+            this.Bounds = componentCapsule;
+            this._buttonBounds = buttonCapsule;
+        }
+
+        protected override void Render (GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            base.Render (canvas, graphics, channel);
+
+            if (channel == GH_CanvasChannel.Objects)
+            {
+                GH_Capsule buttonCapsule = GH_Capsule.CreateTextCapsule
+                (
+                    box: this._buttonBounds,
+                    textbox: this._buttonBounds,
+                    palette: this._isPressed ? GH_Palette.Grey : GH_Palette.Black,
+                    text: "Refresh",
+                    radius: 5,
+                    highlight: 0);
+                buttonCapsule.Render (graphics, this.Selected, this.Owner.Locked, false);
+                buttonCapsule.Dispose ();
+            }
+        }
+
+        public override GH_ObjectResponse RespondToMouseDown (GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (this._buttonBounds.Contains (e.CanvasLocation))
+                {
+                    this._isPressed = true;
+                    sender.Refresh ();
+
+                    return GH_ObjectResponse.Handled;
+                }
+            }
+            return base.RespondToMouseDown (sender, e);
+        }
+
+        public override GH_ObjectResponse RespondToMouseUp (GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (this._buttonBounds.Contains (e.CanvasLocation) && this._isPressed == true)
+                {
+                    if (this.Owner is IButtonComponent buttonComponent)
+                    {
+                        buttonComponent.OnCapsuleButtonPressed();
+                        this._isPressed = false;
+                        sender.Refresh();
+                    }
+                    return GH_ObjectResponse.Release;
+                }
+            }
+            return base.RespondToMouseUp (sender, e);
+        }
+        #endregion Methods
+    }
+
     abstract public class Component : GH_Component
     {
         public Component (string name, string nickname, string description, string subCategory) :
@@ -26,7 +117,7 @@ namespace TapirGrasshopperPlugin.Components
         }
     }
 
-    abstract public class ArchicadAccessorComponent : Component
+    abstract public class ArchicadAccessorComponent : Component, IButtonComponent
     {
         public ArchicadAccessorComponent (string name, string nickname, string description, string subCategory) :
             base (name, nickname, description, subCategory)
@@ -37,6 +128,16 @@ namespace TapirGrasshopperPlugin.Components
         {
             base.AppendAdditionalComponentMenuItems (menu);
             Menu_AppendItem (menu, "Refresh from Archicad", OnRefreshButtonClicked);
+        }
+
+        public override void CreateAttributes ()
+        {
+            this.m_attributes = new ButtonAttributes (this);
+        }
+
+        public virtual void OnCapsuleButtonPressed ()
+        {
+            ExpireSolution (true);
         }
 
         private void OnRefreshButtonClicked (object sender, EventArgs e)
