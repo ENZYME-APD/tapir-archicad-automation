f472cf6aabd3076c7bcbd5206360220949eef610
Viktor Kovacs <viktorkovacs@gmail.com>
2024-11-02 22:57:00 +0100

    Add property full name as an output for easier search with key-value component.

--- grasshopper-plugin/TapirGrasshopperPlugin/Components/PropertiesComponents/GetAllPropertiesComponent.cs
+++ grasshopper-plugin/TapirGrasshopperPlugin/Components/PropertiesComponents/GetAllPropertiesComponent.cs
@@ -27,7 +27,8 @@ namespace TapirGrasshopperPlugin.Components.PropertiesComponents
         {
             pManager.AddGenericParameter ("PropertyId", "PropertyId", "Property id.", GH_ParamAccess.list);
             pManager.AddTextParameter ("GroupName", "GroupName", "Property group name.", GH_ParamAccess.list);
-            pManager.AddTextParameter ("Name", "Name", "Property name.", GH_ParamAccess.list);
+            pManager.AddTextParameter ("PropertyName", "PropertyName", "Property name.", GH_ParamAccess.list);
+            pManager.AddTextParameter ("FullName", "FullName", "Full name containing the group and the property name.", GH_ParamAccess.list);
         }
 
         protected override void SolveInstance (IGH_DataAccess DA)
@@ -42,15 +43,18 @@ namespace TapirGrasshopperPlugin.Components.PropertiesComponents
             List<PropertyIdObj> propertyIds = new List<PropertyIdObj> ();
             List<string> propertyGroupNames = new List<string> ();
             List<string> propertyNames = new List<string> ();
+            List<string> fullNames = new List<string> ();
             foreach (PropertyDetailsObj detail in properties.Properties) {
                 propertyIds.Add (detail.PropertyId);
                 propertyGroupNames.Add (detail.PropertyGroupName);
                 propertyNames.Add (detail.PropertyName);
+                fullNames.Add (ArchicadUtils.JoinNames (detail.PropertyGroupName, detail.PropertyName));
             }
 
             DA.SetDataList (0, propertyIds);
             DA.SetDataList (1, propertyGroupNames);
             DA.SetDataList (2, propertyNames);
+            DA.SetDataList (3, fullNames);
         }
 
         protected override System.Drawing.Bitmap Icon => TapirGrasshopperPlugin.Properties.Resources.GetAllProperties;
