0a3e65a8349374d46e3d958dec942f83a331fde6
tlorantfy <lorantfyt@gmail.com>
2024-11-10 08:37:10 +0100

    Add ClassificationItemPath output to return the full path to classification items in the tree

--- grasshopper-plugin/TapirGrasshopperPlugin/Components/ClassificationsComponents/GetAllClassificationsComponent.cs
+++ grasshopper-plugin/TapirGrasshopperPlugin/Components/ClassificationsComponents/GetAllClassificationsComponent.cs
@@ -24,18 +24,21 @@ namespace TapirGrasshopperPlugin.Components.ClassificationsComponents
         protected override void RegisterOutputParams (GH_OutputParamManager pManager)
         {
             pManager.AddTextParameter ("ClassificationSystemId", "SystemId", "Found Classification System Id.", GH_ParamAccess.list);
-            pManager.AddTextParameter ("ClassificationSystemDisplayId", "SystemDisplayId", "Found Classification System display id.", GH_ParamAccess.list);
+            pManager.AddTextParameter ("ClassificationSystemNameAndVersion", "SystemNameAndVersion", "Found Classification System name and version.", GH_ParamAccess.list);
             pManager.AddTextParameter ("ClassificationItemId", "ItemId", "Found ClassificationItem id.", GH_ParamAccess.list);
             pManager.AddTextParameter ("ClassificationItemDisplayId", "ItemDisplayId", "Found ClassificationItem display id.", GH_ParamAccess.list);
+            pManager.AddTextParameter ("ClassificationItemName", "ItemName", "Found ClassificationItem name.", GH_ParamAccess.list);
+            pManager.AddTextParameter ("ClassificationItemPath", "ItemPath", "Path to ClassificationItem.", GH_ParamAccess.list);
         }
 
-        private List<ClassificationItemDetailsObj> GetAllClassificationItemFromTree (List<ClassificationItemObj> tree)
+        private List<Tuple<ClassificationItemDetailsObj, string>> GetAllClassificationItemFromTree (List<ClassificationItemObj> tree, string pathToRoot = "")
         {
-            List<ClassificationItemDetailsObj> list = new List<ClassificationItemDetailsObj> ();
+            List<Tuple<ClassificationItemDetailsObj, string>> list = new List<Tuple<ClassificationItemDetailsObj, string>> ();
             foreach (ClassificationItemObj item in tree) {
-                list.Add (item.ClassificationItem);
+                string path = pathToRoot + '/' + item.ClassificationItem.Id;
+                list.Add (new Tuple<ClassificationItemDetailsObj, string> (item.ClassificationItem, path));
                 if (item.ClassificationItem.Children != null) {
-                    list.AddRange (GetAllClassificationItemFromTree (item.ClassificationItem.Children));
+                    list.AddRange (GetAllClassificationItemFromTree (item.ClassificationItem.Children, path));
                 }
             }
 
@@ -50,7 +53,7 @@ namespace TapirGrasshopperPlugin.Components.ClassificationsComponents
                 return;
             }
 
-            Dictionary<ClassificationSystemDetailsObj, List<ClassificationItemDetailsObj>> itemsPerSystems = new Dictionary<ClassificationSystemDetailsObj, List<ClassificationItemDetailsObj>> ();
+            Dictionary<ClassificationSystemDetailsObj, List<Tuple<ClassificationItemDetailsObj, string>>> itemsPerSystems = new Dictionary<ClassificationSystemDetailsObj, List<Tuple<ClassificationItemDetailsObj, string>>> ();
             AllClassificationSystems classificationSystems = response.Result.ToObject<AllClassificationSystems> ();
             foreach (ClassificationSystemDetailsObj system in classificationSystems.ClassificationSystems) {
                 ClassificationSystemObj classificationSystem = new ClassificationSystemObj () {
@@ -65,27 +68,36 @@ namespace TapirGrasshopperPlugin.Components.ClassificationsComponents
                 }
 
                 AllClassificationItemsInSystem classificationItemsInSystem = response.Result.ToObject<AllClassificationItemsInSystem> ();
-                List<ClassificationItemDetailsObj> itemsInSystem = GetAllClassificationItemFromTree (classificationItemsInSystem.ClassificationItems);
+                List<Tuple<ClassificationItemDetailsObj, string>> itemsInSystem = GetAllClassificationItemFromTree (classificationItemsInSystem.ClassificationItems);
                 itemsPerSystems.Add (system, itemsInSystem);
             }
 
             List<string> systemIds = new List<string> ();
-            List<string> systemDisplayIds = new List<string> ();
+            List<string> systemNamesAndVersions = new List<string> ();
             List<string> itemIds = new List<string> ();
             List<string> itemDisplayIds = new List<string> ();
-            foreach (KeyValuePair<ClassificationSystemDetailsObj, List<ClassificationItemDetailsObj>> itemsInSystem in itemsPerSystems) {
-                foreach (ClassificationItemDetailsObj itemDetail in itemsInSystem.Value) {
-                    systemIds.Add (itemsInSystem.Key.ClassificationSystemId.Guid);
-                    systemDisplayIds.Add (itemsInSystem.Key.ToString ());
+            List<string> itemNames = new List<string> ();
+            List<string> itemPaths = new List<string> ();
+            foreach (KeyValuePair<ClassificationSystemDetailsObj, List<Tuple<ClassificationItemDetailsObj, string>>> itemsInSystem in itemsPerSystems) {
+                ClassificationSystemDetailsObj system = itemsInSystem.Key;
+                foreach (Tuple<ClassificationItemDetailsObj, string> itemDetailAndPath in itemsInSystem.Value) {
+                    ClassificationItemDetailsObj itemDetail = itemDetailAndPath.Item1;
+                    string itemPath = itemDetailAndPath.Item2;
+
+                    systemIds.Add (system.ClassificationSystemId.Guid);
+                    systemNamesAndVersions.Add (system.ToString ());
                     itemIds.Add (itemDetail.ClassificationItemId.Guid);
-                    itemDisplayIds.Add (itemDetail.ToString ());
+                    itemDisplayIds.Add (itemDetail.Id);
+                    itemNames.Add (itemDetail.Name);
+                    itemPaths.Add (itemPath);
                 }
             }
 
             DA.SetDataList (0, systemIds);
-            DA.SetDataList (1, systemDisplayIds);
+            DA.SetDataList (1, systemNamesAndVersions);
             DA.SetDataList (2, itemIds);
             DA.SetDataList (3, itemDisplayIds);
+            DA.SetDataList (4, itemPaths);
         }
 
         protected override System.Drawing.Bitmap Icon => TapirGrasshopperPlugin.Properties.Resources.AllClassifications;
