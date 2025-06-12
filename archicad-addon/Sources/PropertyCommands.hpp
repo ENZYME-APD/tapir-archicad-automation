d3c6d3a6d9372656faeaeff14cbafda6bde44c54
Dimitris Iliadis <dimitrios.iliadis@outlook.com>
2025-06-12 16:01:14 +0300

    Add Get/SetPropertyValuesOfAttributes

--- archicad-addon/Sources/PropertyCommands.hpp
+++ archicad-addon/Sources/PropertyCommands.hpp
@@ -31,6 +31,26 @@ public:
     virtual GS::ObjectState Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
 };
 
+class GetPropertyValuesOfAttributesCommand : public CommandBase
+{
+public:
+    GetPropertyValuesOfAttributesCommand ();
+    virtual GS::String GetName () const override;
+    virtual GS::Optional<GS::UniString> GetInputParametersSchema () const override;
+    virtual GS::Optional<GS::UniString> GetResponseSchema () const override;
+    virtual GS::ObjectState Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
+};
+
+class SetPropertyValuesOfAttributesCommand : public CommandBase
+{
+public:
+    SetPropertyValuesOfAttributesCommand ();
+    virtual GS::String GetName () const override;
+    virtual GS::Optional<GS::UniString> GetInputParametersSchema () const override;
+    virtual GS::Optional<GS::UniString> GetResponseSchema () const override;
+    // virtual GS::ObjectState Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;    
+};
+
 class CreatePropertyGroupsCommand : public CommandBase
 {
 public:
