05e61f1904d3a424a578398fdc8c98149b7afa67
SzamosiMate <szamimate@yahoo.com>
2025-08-02 10:59:28 +0200

    Added "holes" with "z" coordinates to showcase the effect of this parameter

--- archicad-addon/Examples/create_mesh.py
+++ archicad-addon/Examples/create_mesh.py
@@ -23,6 +23,16 @@ aclib.RunTapirCommand(
                             {'x': x + i, 'y': y + j, 'z': x*0.1 + y*0.1 + i*0.1 + j*0.1 } for i in range(width+1)
                         ]
                     } for j in range(height+1)
+                ],
+                'holes': [
+                        {
+                        'polygonCoordinates': [
+                        {'x': width/2 + x,   'y': height/2 + y,    'z': x*0.2 + y*0.1 },
+                        {'x': width/4 + x, 'y': height/2 + y,    'z': x*0.2 + y*0.1 },
+                        {'x': width/4 + x, 'y': height/4 + y, 'z': x*0.2 + y*0.1 },
+                        {'x': width/2 + x,   'y': height/4 + y, 'z': x*0.2 + y*0.1 }
+                        ],
+                    }
                 ]
             } for x in range(0, nX*(width+gap), (width+gap)) for y in range(0, nY*(height+gap), (height+gap))
         ]
