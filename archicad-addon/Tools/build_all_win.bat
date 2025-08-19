5608550903e249b4f96550c819b6580e1e357819
tlorantfy <lorantfyt@gmail.com>
2025-08-19 12:22:03 +0200

    Prepare for AC29

--- archicad-addon/Tools/build_all_win.bat
+++ archicad-addon/Tools/build_all_win.bat
@@ -3,6 +3,7 @@ python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/r
 python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/26.3000/API.Development.Kit.WIN.26.3000.zip ..\Build\DevKits\AC26
 python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/27.3001/API.Development.Kit.WIN.27.3001.zip ..\Build\DevKits\AC27
 python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/28.3001/API.Development.Kit.WIN.28.3001.zip ..\Build\DevKits\AC28
+python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/29.2001/API.Development.Kit.WIN.29.2001.zip ..\Build\DevKits\AC29
 
 cmake -B ../Build/AC25 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_VERSION=25 -DAC_API_DEVKIT_DIR="..\Build\DevKits\AC25\Support" .. || goto :error
 cmake --build ../Build/AC25 --config Debug || goto :error
@@ -19,6 +20,10 @@ cmake --build ../Build/AC27 --config RelWithDebInfo || goto :error
 cmake -B ../Build/AC28 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_VERSION=28 -DAC_API_DEVKIT_DIR="..\Build\DevKits\AC28\Support" .. || goto :error
 cmake --build ../Build/AC28 --config Debug || goto :error
 cmake --build ../Build/AC28 --config RelWithDebInfo || goto :error
+
+cmake -B ../Build/AC29 -G "Visual Studio 17 2022" -A "x64" -T "v143" -DAC_VERSION=29 -DAC_API_DEVKIT_DIR="..\Build\DevKits\AC29\Support" .. || goto :error
+cmake --build ../Build/AC29 --config Debug || goto :error
+cmake --build ../Build/AC29 --config RelWithDebInfo || goto :error
 
 popd
 exit /b 0
