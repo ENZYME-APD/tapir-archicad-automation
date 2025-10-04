7fa8d6fc62516a71ff82347366af2cabacee312b
tlorantfy <lorantfyt@gmail.com>
2025-10-04 20:40:18 +0200

    Use final AC29 DevKits

--- archicad-addon/Tools/build_all_win.bat
+++ archicad-addon/Tools/build_all_win.bat
@@ -3,7 +3,7 @@ python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/r
 python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/26.3000/API.Development.Kit.WIN.26.3000.zip ..\Build\DevKits\AC26
 python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/27.3001/API.Development.Kit.WIN.27.3001.zip ..\Build\DevKits\AC27
 python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/28.3001/API.Development.Kit.WIN.28.3001.zip ..\Build\DevKits\AC28
-python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/29.2001/API.Development.Kit.WIN.29.2001.zip ..\Build\DevKits\AC29
+python download_and_unzip.py https://github.com/GRAPHISOFT/archicad-api-devkit/releases/download/29.3000/API.Development.Kit.WIN.29.3000.zip ..\Build\DevKits\AC29
 
 cmake -B ../Build/AC25 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_VERSION=25 -DAC_API_DEVKIT_DIR="..\Build\DevKits\AC25\Support" .. || goto :error
 cmake --build ../Build/AC25 --config Debug || goto :error
