3612361a89720f2675542193ccbaf9352113506e
fertegi <ertegi@staab-architekten>
2026-02-22 23:55:53 +0100

    Add example script for getting and setting user data of wall elements

---
+++ archicad-addon/Examples/get_and_set_user_data.py
@@ -0,0 +1,21 @@
+import aclib
+
+
+walls = aclib.RunTapirCommand(
+    "GetElementsByType",
+    {"elementType": "Wall"},
+    debug=False
+)["elements"]
+
+response = aclib.RunTapirCommand(
+    "SetUserDataOfElements",
+    {"elements": walls, "userData": {"foo": "bar"}},
+    debug=True
+)
+
+
+# response = aclib.RunTapirCommand(
+#     "GetUserDataOfElements",
+#     {"elements": walls},
+#     debug=True
+# )
