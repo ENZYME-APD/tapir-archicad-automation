4756f7836f4545cc1e51a97b204a9d02bf20ee47
fertegi <ertegi@staab-architekten>
2026-02-24 13:17:20 +0100

    .

--- archicad-addon/Examples/get_and_set_user_data.py
+++ archicad-addon/Examples/get_and_set_user_data.py
@@ -1,21 +1,56 @@
 import aclib
+import random
 
 
+def pprint(data):
+    import json
+    print(json.dumps(data, indent=2))
+
+
+# some arbitrary data
+data_to_add_template = {
+    "myGuid": "",
+    "someArrayData":
+        [
+        ],
+    "someNestedData": {
+        "key1": "val1",
+        "key2": "val2"
+        }
+}
+
+# get all walls
 walls = aclib.RunTapirCommand(
     "GetElementsByType",
     {"elementType": "Wall"},
     debug=False
 )["elements"]
 
-response = aclib.RunTapirCommand(
-    "SetUserDataOfElements",
-    {"elements": walls, "userData": {"foo": "bar"}},
-    debug=True
-)
 
+# this do not work for now,
+# only one userData obj will be set to all elements
+datas_to_add = []
+
+for wall in walls:
+    d = data_to_add_template.copy()
+    d["myGuid"] = f"CUSTOM_{wall.get("elementId", {}).get("guid")}"
+    d["someMetaDataAsArray"] = [random.randint(0, 50) for i in range(3)]
+    datas_to_add.append(d)
+
+# Currently, only one metadata will be applied to all elements, need to change that, so we can specify for each element custom data.
+# response = aclib.RunTapirCommand(
+#     "SetUserDataOfElements",
+#     {"elements": walls, "userData": datas_to_add[0]},
+#     debug=False
+# )
+
+# pprint(response)
 
 response = aclib.RunTapirCommand(
     "GetUserDataOfElements",
     {"elements": walls},
-    debug=True
+    debug=False
 )
+
+
+pprint(response)
