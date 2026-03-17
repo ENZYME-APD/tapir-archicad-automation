ed21690e0f78e47723dd7f298dba8c1b759c0307
tlorantfy <lorantfyt@gmail.com>
2026-03-17 22:19:36 +0100

    New GH components: SaveProject and ZoomToElements

---
+++ grasshopper-plugin/TapirGrasshopperPlugin/Components/ElementsComponents/ZoomToElementsComponent.cs
@@ -0,0 +1,57 @@
+﻿using Grasshopper.Kernel;
+using System;
+using TapirGrasshopperPlugin.Helps;
+using TapirGrasshopperPlugin.Types.Element;
+using TapirGrasshopperPlugin.Types.Generic;
+
+namespace TapirGrasshopperPlugin.Components.ElementsComponents
+{
+    public class ZoomToElementsComponent : ArchicadExecutorComponent
+    {
+        public override string CommandName => "FitInWindow";
+
+        public ZoomToElementsComponent()
+            : base(
+                "ZoomToElements",
+                "Zoom to the given elements. If no element is given, it works as 'fit in window', zooms to all elements in the project.",
+                GroupNames.Elements)
+        {
+        }
+
+        protected override void AddInputs()
+        {
+            InGenerics(
+                "ElementGuids",
+                "Element ids to zoom to.");
+        }
+
+        protected override void Solve(
+            IGH_DataAccess da)
+        {
+            if (!da.TryCreateFromList(
+                    0,
+                    out ElementsObject input))
+            {
+                return;
+            }
+
+            if (!TryGetConvertedCadValues(
+                    CommandName,
+                    input,
+                    ToAddOn,
+                    ExecutionResult.Deserialize,
+                    out ExecutionResult response))
+            {
+                return;
+            }
+
+            if (!response.Success)
+            {
+                this.AddError(response.Message());
+            }
+        }
+
+        public override Guid ComponentGuid =>
+            new Guid("30bf1eeb-6542-4d37-81ea-20d5cff55d72");
+    }
+}
---
+++ grasshopper-plugin/TapirGrasshopperPlugin/Components/ProjectComponents/SaveProjectComponent.cs
@@ -0,0 +1,52 @@
+﻿using Grasshopper.Kernel;
+using System;
+using TapirGrasshopperPlugin.Helps;
+using TapirGrasshopperPlugin.Types.Generic;
+
+namespace TapirGrasshopperPlugin.Components.ProjectComponents
+{
+    public class SaveProjectComponent : ArchicadAccessorComponent
+    {
+        public override string CommandName => "SaveProject";
+
+        public SaveProjectComponent()
+            : base(
+                "SaveProject",
+                "Saves the current project.",
+                GroupNames.Project)
+        {
+        }
+
+        protected override void AddInputs()
+        {
+        }
+
+        protected override void AddOutputs()
+        {
+            OutText(
+                nameof(ExecutionResult.Message),
+                ExecutionResult.Doc);
+        }
+
+        protected override void Solve(
+            IGH_DataAccess da)
+        {
+            if (!TryGetConvertedCadValues(
+                    CommandName,
+                    null,
+                    ToAddOn,
+                    ExecutionResult.Deserialize,
+                    out ExecutionResult response))
+            {
+                return;
+            }
+
+            da.SetData(
+                0,
+                response.Message());
+        }
+
+        public override Guid ComponentGuid =>
+            new Guid("8a6d3473-d937-433f-80b2-13b0f07ca669");
+    }
+}
