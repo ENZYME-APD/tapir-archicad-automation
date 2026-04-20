7b0b3c9e331eb3fda076f4069f92c7afa61d2c30
szamosi.mate <szamimate@yahoo.com>
2026-04-20 17:55:23 +0200

    - Moved Create Groups to ElementGroupingCommands - added detailed error messages - the elements list can now contain element and group guids - it is now only avalible for archicad 27 +

--- archicad-addon/Sources/ElementCommands.hpp
+++ archicad-addon/Sources/ElementCommands.hpp
@@ -167,13 +167,3 @@ public:
     virtual GS::Optional<GS::UniString> GetResponseSchema () const override;
     virtual GS::ObjectState Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
 };
-
-class CreateGroupsCommand : public CommandBase
-{
-public:
-    CreateGroupsCommand ();
-    virtual GS::String GetName () const override;
-    virtual GS::Optional<GS::UniString> GetInputParametersSchema () const override;
-    virtual GS::Optional<GS::UniString> GetResponseSchema () const override;
-    virtual GS::ObjectState Execute (const GS::ObjectState& parameters, GS::ProcessControl& processControl) const override;
-};
